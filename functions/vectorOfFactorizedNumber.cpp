ae144568b6030dc7fba56acdfdf8f9c62cc4e2df
llollcat <llollcat@yandex.ru>
2021-11-27 14:20:36 +0300

    Update vectorOfFactorizedNumber.cpp

--- functions/vectorOfFactorizedNumber.cpp
+++ functions/vectorOfFactorizedNumber.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 
 vector<long long> vectorOfFactorizedNumber(long long number){
-    vector<long long> factors;
+    vector<long long> factorized;
     for (long long i = 2;i <= static_cast<long long>(sqrt(number)); ++i){
         while (number % i == 0){
-            factors.push_back(i);
+            factorized.push_back(i);
             number /= i;
         }
     }
-    if ((number != 1) || (factors.empty())){
-        factors.push_back(number);
+    if ((number != 1) || (factorized.empty())){
+        factorized.push_back(number);
     }
-    return factors;
+    
+    return factorized;
 } 
